e5b4a83919f6bf89ce3a269bb964cbae51a697dd
Your Name <you@example.com>
2023-11-21 19:07:17 -0500

    Definicion de rutas

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/saludo', function () {
+    return "Este es un saludo desde Laravel";
+});
+
+
+
+Route::get('/galeria/{num}', function ($num) {
+    return "Este es un saludo desde Laravel ".$num;
+}) -> where ('num', '[0-9]+');
+
+
+Route::view('/galeria', 'pagGaleria', ['valor'=>15]) -> name('xGaleria'); 
+
+Route::get('/lista', function () {
+    return view('pagLista');
+}) -> name('xLista');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
